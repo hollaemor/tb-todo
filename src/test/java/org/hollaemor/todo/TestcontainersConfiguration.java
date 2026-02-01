cf23fd27fb7a55e5023959e7201f4b6fa81e867b
Emmanuel Oriarewo <hollaemor@gmail.com>
2026-02-01 23:30:54 +0100

    Remove references to test containers.
    Also remove irrelevant JSON test.

--- src/test/java/org/hollaemor/todo/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.hollaemor.todo;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
